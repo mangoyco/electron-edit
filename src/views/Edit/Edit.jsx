8bc68c2a331d343be0d3350b8177077a50b54205
mangoyco <810623295@qq.com>
2020-10-21 13:36:42 +0800

    basic md

--- src/views/Edit/Edit.jsx
+++ src/views/Edit/Edit.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import './Edit.css'
+import './md.css'
+import marked from 'marked'
 
 class Edit extends React.Component{
+
+  constructor() {
+    super()
+    this.state = {
+      inputText: '',
+      previewHtml:null
+    }
+  }
+
   render() {
     return (
-      <div>
-        <div className="container edit_area" contentEditable>
-
+      <div className="outer_container">
+        <textarea onChange={this.inputChange} className="edit_area" value={this.state.inputText} spellCheck="false">
+        </textarea>
+        <div id="preview" className="markdown-body" dangerouslySetInnerHTML={{__html:this.state.previewHtml }}>
         </div>
       </div>
     )
-   }
+  }
+
+  inputChange = (e) => {
+    const inp = e.target.value
+    const html = marked(inp)
+    this.setState({ inputText: inp, previewHtml:html}, () => { console.log(this.state) })
+  }
 }
 
 export default Edit
